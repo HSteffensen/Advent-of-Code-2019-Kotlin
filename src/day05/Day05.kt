2ed7d3a803070e8bdc73d740fb3b9bd3c2bb5bfb
Henry Steffensen <hsteffensen@bol.com>
2021-12-20 15:19:00 +0100

    day 5

---
+++ src/day05/Day05.kt
@@ -0,0 +1,55 @@
+package day05
+
+import finalAnswerIsNotWrong
+import intcode.Computer
+import intcodeInput
+import readInput
+import testAnswer
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    fun part1(input: List<Int>): Int =
+        Computer(input).runWithInput(1).output.toList()
+            .single { it != 0 }
+
+    fun part2(input: List<Int>): Int =
+        Computer(input).runWithInput(5).output.toList()
+            .single { it != 0 }
+
+
+
+    // test if implementation meets criteria from the description, like:
+//    val testInput = intcodeInput(readInput("day05/test"))
+    testAnswer(Computer(intcodeInput("1002,4,3,4,33")).run().program[4], 99).also { println("Test part 1 passed") }
+    testAnswer(Computer(intcodeInput("1101,100,-1,4,0")).run().program[4], 99).also { println("Test part 1 passed") }
+//    testAnswer(part2(testInput), 0).also { println("Test part 2 passed") }
+    // < and = tests
+    testAnswer(Computer(intcodeInput("3,9,8,9,10,9,4,9,99,-1,8")).runWithInput(8).output.read(), 1).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,9,8,9,10,9,4,9,99,-1,8")).runWithInput(0).output.read(), 0).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,9,7,9,10,9,4,9,99,-1,8")).runWithInput(8).output.read(), 0).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,9,7,9,10,9,4,9,99,-1,8")).runWithInput(0).output.read(), 1).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,3,1108,-1,8,3,4,3,99")).runWithInput(8).output.read(), 1).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,3,1108,-1,8,3,4,3,99")).runWithInput(0).output.read(), 0).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,3,1107,-1,8,3,4,3,99")).runWithInput(8).output.read(), 0).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,3,1107,-1,8,3,4,3,99")).runWithInput(0).output.read(), 1).also { println("Test part 2 passed") }
+    // jump tests
+    testAnswer(Computer(intcodeInput("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9")).runWithInput(8).output.read(), 1).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9")).runWithInput(0).output.read(), 0).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,3,1105,-1,9,1101,0,0,12,4,12,99,1")).runWithInput(8).output.read(), 1).also { println("Test part 2 passed") }
+    testAnswer(Computer(intcodeInput("3,3,1105,-1,9,1101,0,0,12,4,12,99,1")).runWithInput(0).output.read(), 0).also { println("Test part 2 passed") }
+
+    val input = intcodeInput(readInput("day05/input"))
+    val wrongPart1Answers = listOf<Int>(
+        3,
+        0
+    )
+    measureTimeMillis {
+        println("Part 1: ${finalAnswerIsNotWrong(part1(input), wrongPart1Answers)}")
+    }.also { println("\ttook $it milliseconds") }
+
+    val wrongPart2Answers = listOf<Int>(
+    )
+    measureTimeMillis {
+        println("Part 2: ${finalAnswerIsNotWrong(part2(input), wrongPart2Answers)}")
+    }.also { println("\ttook $it milliseconds") }
+}
--- src/intcode/Computer.kt
+++ src/intcode/Computer.kt
@@ -1,20 +1,94 @@
 package intcode
 
+import kotlin.collections.ArrayDeque
+
+class CommunicationChannel {
+    private var values = ArrayDeque<Int>()
+    val isEmpty: Boolean
+        get() = values.isEmpty()
+    fun read(): Int {
+        check(!isEmpty) { "Can only read if channel is not empty" }
+        return values.removeFirst()
+    }
+    fun write(value: Int) {
+        values.addLast(value)
+    }
+    fun toList() = values.toList()
+}
+
 class Computer(
-    initialProgram: List<Int>
+    initialProgram: List<Int>,
+    val input: CommunicationChannel,
+    val output: CommunicationChannel
 ) {
+    constructor(initialProgram: List<Int>) : this(initialProgram, CommunicationChannel(), CommunicationChannel())
     val program: MutableList<Int> = initialProgram.toMutableList()
     var pointer: Int = 0
+    val completed: Boolean
+        get() = program[pointer] == 99
+    val awaitingInput: Boolean
+        get() = program[pointer] == 3 && input.isEmpty
+
+    private fun getValueAt(position: Int, isImmediate: Boolean): Int =
+        if (isImmediate)
+            program[position]
+        else
+            program[program[position]]
+
+    private fun setValueAt(position: Int, value: Int) {
+        program[program[position]] = value
+    }
+
+    private fun setValueAt(position: Int, valueFun: () -> Int) {
+        setValueAt(position, valueFun())
+    }
+
+    private fun firstArg() =
+        getValueAt(pointer + 1, (program[pointer] / 100) % 10 == 1)
+
+    private fun secondArg() =
+        getValueAt(pointer + 2, (program[pointer] / 1000) % 10 == 1)
+
+    private fun thirdArg() =
+        getValueAt(pointer + 3, (program[pointer] / 10000) % 10 == 1)
 
     fun step(): Int {
-        return when (program[pointer]) {
+        return when (program[pointer] % 100) {
             99 -> pointer
             1 -> {
-                program[program[pointer+3]] = program[program[pointer+1]] + program[program[pointer+2]]
+                setValueAt(pointer+3) { firstArg() + secondArg() }
                 pointer + 4
             }
             2 -> {
-                program[program[pointer+3]] = program[program[pointer+1]] * program[program[pointer+2]]
+                setValueAt(pointer+3) { firstArg() * secondArg() }
+                pointer + 4
+            }
+            3 -> {
+                setValueAt(pointer+1, input.read())
+                pointer + 2
+            }
+            4 -> {
+                output.write(firstArg())
+                pointer + 2
+            }
+            5 -> {
+                if (firstArg() != 0)
+                    secondArg()
+                else
+                    pointer + 3
+            }
+            6 -> {
+                if (firstArg() == 0)
+                    secondArg()
+                else
+                    pointer + 3
+            }
+            7 -> {
+                setValueAt(pointer+3) { if (firstArg() < secondArg()) 1 else 0 }
+                pointer + 4
+            }
+            8 -> {
+                setValueAt(pointer+3) { if (firstArg() == secondArg()) 1 else 0 }
                 pointer + 4
             }
             else -> TODO()
@@ -26,11 +100,21 @@ class Computer(
     }
 
     fun run(debug: Boolean = false): Computer {
-        while (program[pointer] != 99) {
+        while (!completed && !awaitingInput) {
             if (debug) debugPrint()
             pointer = step()
         }
         if (debug) debugPrint()
         return this
     }
+
+    fun runWithInput(args: List<Int>, debug: Boolean = false): Computer {
+        args.forEach {
+            input.write(it)
+        }
+        return run(debug)
+    }
+
+    fun runWithInput(arg: Int, debug: Boolean = false): Computer =
+        runWithInput(listOf(arg), debug)
 }
